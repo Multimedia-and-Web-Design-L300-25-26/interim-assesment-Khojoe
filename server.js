63cfbc8fc4a200fcec74e4e9bf0703f30845f94c
Immanuel Oheneba Debe <immanueldebe@gmail.com>
2026-05-02 12:08:40 +0000

    Update CORS for production

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port, change if necessary
-    credentials: true, // Allow cookies to be sent with requests
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174'], 
+    credentials: true, 
 }));
 app.use(express.json());
 app.use(cookieParser());
