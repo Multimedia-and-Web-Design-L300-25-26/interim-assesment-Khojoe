dab8341aade15ff6832746717c793bf837834e86
Immanuel Oheneba Debe <immanueldebe@gmail.com>
2026-05-04 22:13:17 +0000

    Add root and api health check routes

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
+app.get('/', (req, res) => {
+    res.send("API is running!");
+});
 app.get('/api', (req, res) => {
     res.send("Welcome to the Crypto App API!" );
 });
