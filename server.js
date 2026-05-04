1229768de309f03b6d3ce2d9389601be68e173ba
Immanuel Oheneba Debe <immanueldebe@gmail.com>
2026-05-04 22:00:37 +0000

    added a return message to show the backend is running

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174'], 
+    origin: [process.env.FRONTEND_URL], 
     credentials: true, 
 }));
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
+app.get('/api', (req, res) => {
+    res.json({ message: "Welcome to the Crypto App API!" });
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/crypto', cryptoRoutes);
 
